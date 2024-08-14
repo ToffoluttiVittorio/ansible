d6cec7e06af8e0ff5729bcbe10d3e3a953c1d90c
ToffoluttiVittorio <97800904+ToffoluttiVittorio@users.noreply.github.com>
2024-08-14 14:56:42 +0400

    Update other.sh

--- Configuration/other.sh
+++ Configuration/other.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Mise à jour du fichier JSON pour les projections
-echo "Ajout de la nouvelle entrée à la liste 'projections' dans le fichier JSON..."
-sed -i '/"projections": \[/a \
+# Vérifier si la nouvelle projection existe déjà dans le fichier JSON et ajouter si elle n'existe pas
+echo "Vérification et ajout de la nouvelle entrée à la liste 'projections' dans le fichier JSON..."
+if ! grep -q '"value": "EPSG:2975"' /etc/georchestra/datafeeder/frontend-config.json; then
+    sed -i '/"projections": \[/a \
     {\
-      "label": "New Projection",\
+      "label": "RGR92 / UTM zone 40S",\
       "value": "EPSG:2975"\
     },' /etc/georchestra/datafeeder/frontend-config.json
+    echo "Nouvelle entrée ajoutée à la liste 'projections'."
+else
+    echo "La projection 'EPSG:2975' existe déjà dans la liste 'projections'."
+fi
+
 echo "Mise à jour du fichier JSON terminée."
 
 # Remplacement des valeurs dans le fichier XML
-echo "Remplacement de 'codeListValue=\"eng\"' par 'codeListValue=\"fre\"' dans le fichier XML..."
-sed -i 's/codeListValue="eng"/codeListValue="fre"/g' /etc/georchestra/datafeeder/metadata.xml
+echo "Remplacement de 'codeListValue=\"eng\"' par 'codeListValue=\"fre\"' dans le fichier XML de datafeeder"
+sed -i 's/codeListValue="eng"/codeListValue="fre"/g' /etc/georchestra/datafeeder/metadata_template.xml
 echo "Remplacement dans le fichier XML terminé."
 
 # Suppression du fichier header_bg.web et copie du fichier header_bg.webp
@@ -22,7 +28,7 @@ echo "Fichier header_bg.web remplacé par header_bg.webp."
 
 # Remplacement dans le fichier TOML pour les langues
 echo "Remplacement de '# languages = ['en', 'fr', 'de']' par 'languages = ['en', 'fr', 'de']' dans le fichier TOML..."
-sed -i 's/# languages = \[\'en\', \'fr\', \'de\'\]/languages = \[\'en\', \'fr\', \'de\'\]/' /etc/georchestra/datahub/conf/default.toml
+sed -i "s/# languages = \['en', 'fr', 'de'\]/languages = \['en', 'fr', 'de'\]/" /etc/georchestra/datahub/conf/default.toml
 echo "Remplacement dans le fichier TOML terminé."
 
 # Remplacement de la couleur primaire dans le fichier TOML
@@ -36,26 +42,28 @@ sed -i 's/secondary_color = "#1b1f3b"/secondary_color = "#225ea8"/' /etc/georche
 echo "Remplacement de la couleur secondaire dans le fichier TOML terminé."
 
 # Suppression du commentaire et activation de la ligne dans le fichier TOML
-echo "Remplacement de '# enabled = true' par 'enabled = true' dans le fichier TOML..."
+echo "Remplacement de '# enabled = true' par 'enabled = true' pour activer le % de qualité de métadonnée"
 sed -i 's/# enabled = true/enabled = true/' /etc/georchestra/datahub/conf/default.toml
 echo "Activation de la ligne dans le fichier TOML terminée."
 
-# Suppression d'une section spécifique dans le fichier JSON
-echo "Suppression des sections 'en' et 'it' dans le fichier JSON..."
-sed -i '/"en": {/,/description": "English"/{N;N;N;d;}}' /etc/georchestra/mapstore/configs/localConfig.json
-sed -i '/"it": {/,/description": "Italiano"/{N;N;N;d;}}' /etc/georchestra/mapstore/configs/localConfig.json
+# Suppression des sections 'en' et 'it' dans le fichier JSON
+#echo "Suppression des sections 'en' et 'it' dans le fichier JSON..."
+#sed -i '/"en": {/,/},/d' /etc/georchestra/mapstore/configs/localConfig.json
+#sed -i '/"it": {/,/},/d' /etc/georchestra/mapstore/configs/localConfig.json
+#echo "Suppression des sections terminée."
 
-# Nettoyage des virgules résiduelles
-echo "Nettoyage des virgules résiduelles..."
-sed -i '/},/d' /etc/georchestra/mapstore/configs/localConfig.json
-
-# Ajouter la nouvelle projection à la section 'projectionDefs'
-echo "Ajout de la nouvelle projection à la section 'projectionDefs'..."
-sed -i '/"projectionDefs": \[/a \
+# Vérifier si la nouvelle projection existe déjà dans la section 'projectionDefs' et ajouter si elle n'existe pas
+echo "Vérification et ajout de la nouvelle projection à la section 'projectionDefs'..."
+if ! grep -q '"code": "EPSG:2975"' /etc/georchestra/mapstore/configs/localConfig.json; then
+    sed -i '/"projectionDefs": \[/a \
       "code": "EPSG:2975",\
       "def": "+proj=lcc +lat_1=48.5 +lat_2=49.5 +lat_0=48.0 +lon_0=-123.0 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",\
       "extent": [-600000, 1500000, 1200000, 5000000],\
       "worldExtent": [-130, 24, -66, 49]\
-    },' /etc/georchestra/mapstore/configs/localConfig.json
+    },{' /etc/georchestra/mapstore/configs/localConfig.json
+    echo "Nouvelle projection ajoutée à la section 'projectionDefs'."
+else
+    echo "La projection 'EPSG:2975' existe déjà dans la section 'projectionDefs'."
+fi
 
-echo "Ajout de la projection terminé."
+echo "Mise à jour du fichier terminé."
