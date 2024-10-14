849fda58d0a695b157e04f696b93071d4771aa57
root <root@dev-carto.ole.re>
2024-10-14 09:37:24 +0000

    Mise à jour du script de remplacement

--- Configuration/script_remplacement.sh
+++ Configuration/script_remplacement.sh
@@ -29,3 +29,25 @@ echo "Mise à jour de la timezone terminée."
 echo "Remplacement des valeurs de orgTypeValues par leur traduction en français..."
 sed -i 's/orgTypeValues=Association,Company,NGO,Individual,Other/orgTypeValues=Association,Entreprise,ONG,Individu,Autre/' /etc/georchestra/console/console.properties
 echo "Traduction des valeurs de orgTypeValues terminée."
+
+
+# Ajout de l'option print dans Mapstore
+CONFIG_FILE="/srv/tomcat/proxycas/bin/setenv.sh"
+# Vérifier si la ligne JAVA_OPTS="$JAVA_OPTS -DPRINT_BASE_URL=pdf" existe déjà
+if grep -q 'JAVA_OPTS="$JAVA_OPTS -DPRINT_BASE_URL=pdf"' "$CONFIG_FILE"; then
+    echo "La ligne JAVA_OPTS est déjà présente dans le fichier."
+else
+    echo "Ajout de la ligne JAVA_OPTS dans le fichier."
+    echo 'JAVA_OPTS="$JAVA_OPTS -DPRINT_BASE_URL=pdf"' >> "$CONFIG_FILE"
+    echo "Ligne ajoutée avec succès."
+fi
+
+# Modification du zoom et du centre dans le fichier de configuration
+echo "Modification du zoom et du centre dans le fichier /etc/georchestra/mapstore/configs/config.json..."
+# Remplacer le zoom par 9
+sed -i 's/"zoom": [0-9]\+\,/"zoom": 9\,/g' "/etc/georchestra/mapstore/configs/config.json"
+# Remplacer les coordonnées X du centre par 651719.38
+sed -i 's/"x": [0-9\.]\+\,/"x": 651719.38\,/g' "/etc/georchestra/mapstore/configs/config.json"
+# Remplacer les coordonnées Y du centre par -2223657.28
+sed -i 's/"y": [-0-9\.]\+\,/"y": -2223657.28\,/g' "/etc/georchestra/mapstore/configs/config.json"
+echo "Le zoom et le centre de la carte ont été mis à jour."
